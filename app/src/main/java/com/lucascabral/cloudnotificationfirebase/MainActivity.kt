a16f6d046ec8a9e464b3f4ff06439a21f618757a
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-07-07 09:32:55 -0300

    Notificação criada pelo sistema android com sucesso.

--- app/src/main/java/com/lucascabral/cloudnotificationfirebase/MainActivity.kt
+++ app/src/main/java/com/lucascabral/cloudnotificationfirebase/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.lucascabral.cloudnotificationfirebase
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.lucascabral.cloudnotificationfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        createNotificationChannel()
+        showNotificationListener()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                lightColor = Color.RED
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotificationListener() {
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        binding.mainShowNotification.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID, buildNotification())
+        }
+    }
+
+    private fun buildNotification(): Notification {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Content Title Notification")
+            .setContentText("Content Text Notification")
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+    }
+
+    companion object {
+        const val CHANNEL_ID = "CHANNEL_ID"
+        const val CHANNEL_NAME = "CHANNEL_NAME"
+        const val NOTIFICATION_ID = 0
     }
 }
